0e358dbaa05e74a3dc21cf0ffde9856258d9f762
caca.py <72211651+dontbqn@users.noreply.github.com>
2022-12-19 14:58:40 +0100

    minor update
    coats of arms : must add popovers features and by figuring out how to display them over the area coordinates.
    Timeline : must find in my brain what I could add more

--- js/alreadyin.js
+++ js/alreadyin.js
@@ -1,12 +1,20 @@
+/* Récupère l'URL, vérifie si c'est la même page que l'utilisateur a cliqué, et envoie une alerte js /
+ou un modal bootstrap 5 (équivalent) 
+> https://getbootstrap.com/docs/5.0/components/modal/
+
+
 window.onload = function {isalready()};
 
+const actual = document.getElement => récupère le lien ayan l'attribut : aria-current="page"
 
 function isalready() {
     elt = document.getElementsByTagName("a");
     elt.addEventListener('click', function(x) {
-        if (x.href == actual){
+        if (x.aria-current == page){                   => si le lien cliqué possede l'attribut ariacurrent:"page", c'est que l'utilisateur est deja sur la page
+                                                        Autrement on on regarderait si l'attribut href == "#"
             alert("vous êtes déjà sur cette page");
         }
     });
 }
-     
+
+*/
