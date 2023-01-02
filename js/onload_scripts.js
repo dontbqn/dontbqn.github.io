2e7e7c9d7e1de5443d0a1a326744ecc6a2532581
cacolac.py <cricoadrien@gmail.com>
2023-01-02 16:06:51 +0100

    Final update 2
    fully responsive except for weird lookin contact form and the non-responsive arm of coats

--- js/onload_scripts.js
+++ js/onload_scripts.js
@@ -47,19 +47,30 @@ function NavigBarOut() {
 //loading spinner when loading the window
 function loading(){
     document.getElementById("prnm_nm").innerHTML = "<div class='float-end'><div class='spinner-border text-light' style='width:3rem; height: 3rem;' role='status'><span class='visually-hidden'></span></div></div>";
+
     document.addEventListener('wheel', (event) => { // scroll up/down cancels loading spinner
         stoploading();
     });
     entete = document.getElementById("header"); 
-    entete.addEventListener('mouseover', (event) => { // Mouse over Header cancels loading spinner
+    entete.addEventListener('mouseover', (event) => { // Mouse over the Header cancels loading spinner
         stoploading();
     });
-
 }
 
 function stoploading(){ // replace spinner by Adrien Crico's header 1
     document.getElementById("entete").innerHTML = "<div class='jumbotron mt-4 text-light' id='entete'><h1 class='display-4' id='prnm_nm'>Adrien Crico</h1></div>";
 }
+/*
+    When the user switch to another application, loading function is called back
+*/
+window.addEventListener("blur", () => {
+    loading();
+});
+// when the user is back to the website
+window.addEventListener("focus", () => {
+    stoploading();
+});
+
 
 
 //still no search..
