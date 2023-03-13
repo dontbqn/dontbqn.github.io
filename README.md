2a3db8e1a51c44fbcd0df37939ce983f8ba1887d
cacolac.py <cricoadrien@gmail.com>
2023-03-13 20:55:03 +0100

    navbar appears on hover....

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # dontbqn.github.io
 
-Conservé du dernier site (SAE14 se présenter sur Interrnet) : Blog, ContactForm, CV, Index, Timeline.
+Conservé : blog, contact, CV, index prk pas, Timeline
 
 Future updates : 
-  - add a real searchbar feature 
-  - add php program to extract content of the contact's form, so I can actually receive mails
+  - change every images because since now I've been using people images without their consent. eh.
+  - javascript function to alert user when they try to navigate to a page they're already in.
+  - add searchbar feature 
+  - add php program to extract content of the contact's form
   - ensure that the mobile version is correctly designed and responsive
   - timeline popups cards appearing at a certain scrollY
+  - 
--- js/onload_scripts.js
+++ js/onload_scripts.js
@@ -11,49 +11,75 @@ function openToast(){
 }
 //NavigationBar Animation
 function navbarAnimState() {
+
     if (document.documentElement.scrollTop < 150){
         document.getElementById("header").style.visibility = "visible";
     }
-    BtnNav = document.getElementById("btnNav");
-    BtnNav.addEventListener("mouseover", (e) => {
-        NavigBarOver();
+
+    const btnNav = document.getElementById("btnNav");
+    const navbarCollapse = document.getElementById("navbarToggleCont");
+    const droppy = document.getElementById("O_droppy");
+
+    navbarCollapse.addEventListener('hidden.bs.collapse', (event) => {
+       bootstrap.Dropdown.getInstance(droppy).hide();
+      });
+    btnNav.addEventListener("mouseover", function() {
+       NavigBarOver();
     });
-    BtnNav.addEventListener("click", (e) => {
+
+    btnNav.addEventListener("click", (e) => {
         NavigBarClick();
     });
 
-    BtnNav.addEventListener("mouseleave", (e) => {
-        NavigBarOut();
-    });
+    droppy.addEventListener("mouseover", function() {
+        if (droppy.classList.contains("show")) {
+            bootstrap.Dropdown.getInstance(droppy).hide();
+            
+        }else {
+            if(droppy){
+                bootstrap.Dropdown.getOrCreateInstance(droppy).show();
+            }
+        }
+     });
 }
 
-let collapsed = false;
 function NavigBarOver() {
-    if (collapsed){
-        document.getElementById("btnNav").innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-caret-up-square' viewBox='0 0 16 16'><path d='M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z'/><path d='M3.544 10.705A.5.5 0 0 0 4 11h8a.5.5 0 0 0 .374-.832l-4-4.5a.5.5 0 0 0-.748 0l-4 4.5a.5.5 0 0 0-.082.537z'/></svg>";
+
+    const btnNav = document.getElementById("btnNav");
+    const navbarCollapse = document.getElementById("navbarToggleCont");
+
+    if (!navbarCollapse.classList.contains("show")){
+        btnNav.innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-caret-up-fill' viewBox='0 0 16 16'><path d='m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z'/><span class='visually-hidden'>O</span></svg>";
+
+        //moche haut btnNav.innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-caret-up-square' viewBox='0 0 16 16'><path d='M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z'/><path d='M3.544 10.705A.5.5 0 0 0 4 11h8a.5.5 0 0 0 .374-.832l-4-4.5a.5.5 0 0 0-.748 0l-4 4.5a.5.5 0 0 0-.082.537z'/></svg>";
     }else{
-        document.getElementById("btnNav").innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-caret-down-square' viewBox='0 0 16 16'><path d='M3.626 6.832A.5.5 0 0 1 4 6h8a.5.5 0 0 1 .374.832l-4 4.5a.5.5 0 0 1-.748 0l-4-4.5z'/><path d='M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2z'/><span class='visually-hidden'>O</span></svg>";
+        btnNav.innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-caret-down-fill' viewBox='0 0 16 16'><path d='M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z'/><span class='visually-hidden'>O</span></svg>";
+
+        //moche bas btnNav.innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-caret-down-square' viewBox='0 0 16 16'><path d='M3.626 6.832A.5.5 0 0 1 4 6h8a.5.5 0 0 1 .374.832l-4 4.5a.5.5 0 0 1-.748 0l-4-4.5z'/><path d='M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2z'/><span class='visually-hidden'>O</span></svg>";
+    }
+    
+    if (navbarCollapse.classList.contains("show")) {
+        bootstrap.Collapse.getInstance(navbarCollapse).hide();
+        
+    }else {
+        if(navbarCollapse){
+            bootstrap.Collapse.getOrCreateInstance(navbarCollapse).show();
+        }
     }
 }
 
 function NavigBarClick() {
-    if (collapsed){
-        document.getElementById("btnNav").innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-caret-down-fill' viewBox='0 0 16 16'><path d='M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z'/><span class='visually-hidden'>O</span></svg>";
-        collapsed = false;
+    const btnNav = document.getElementById("btnNav");
+    const navbarCollapse = document.getElementById("navbarToggleCont");
+
+    if (!navbarCollapse.classList.contains("show")){
+        btnNav.innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-caret-down-fill' viewBox='0 0 16 16'><path d='M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z'/><span class='visually-hidden'>O</span></svg>";
     }else{
-        document.getElementById("btnNav").innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-caret-up-fill' viewBox='0 0 16 16'><path d='m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z'/><span class='visually-hidden'>O</span></svg>";
-        collapsed = true;
-    }
-}
-function NavigBarOut() {
-    if (collapsed == false){
-        document.getElementById("btnNav").innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-caret-down-fill' viewBox='0 0 16 16'><path d='M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z'/><span class='visually-hidden'>O</span></svg>";
-    }else{
-        document.getElementById("btnNav").innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-caret-up-fill' viewBox='0 0 16 16'><path d='m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z'/><span class='visually-hidden'>O</span></svg>";
+        btnNav.innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-caret-up-fill' viewBox='0 0 16 16'><path d='m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z'/><span class='visually-hidden'>O</span></svg>";
     }
 }
 
-//loading spinner when loading the window
+//Loading spinner spawning when the window loads
 function loading(){
     document.getElementById("prnm_nm").innerHTML = "<div class='float-end'><div class='spinner-border text-black-50' style='width:3rem; height: 3rem;' role='status'><span class='visually-hidden'></span></div></div>";
     document.addEventListener('wheel', (event) => { // scroll up/down cancels loading spinner
@@ -67,8 +93,8 @@ function loading(){
     let start = Date.now(); // returns the number of milliseconds since January 1, 1970
     setInterval(function() {
     let timePassed = Date.now() - start; // time setter
-    if (timePassed > 2300){ 
-        stoploading(); // stop the spinner after 2,3 seconds
+    if (timePassed > 1000){ 
+        stoploading(); // stop the spinner after 1 second
     }
     }, 1); // 1ms speed
 }
